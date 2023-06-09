4d55f156d7c279c5a240cf088731d6ffaa016939
Svet <125960896+akiiyamas@users.noreply.github.com>
2023-06-09 15:58:04 +0300

    Update visual_novel.h

--- visual_novel.h
+++ visual_novel.h
@@ -1,1 +1,32 @@
+#ifndef VISUAL_NOVEL_H
+#define VISUAL_NOVEL_H
 
+#include <string>
+#include <vector>
+
+// Structure for representing a choice
+struct Choice {
+    std::string text;
+    int nextIndex;
+};
+
+// Structure for representing a visual novel scene
+struct Scene {
+    std::string dialogue;
+    std::string imagePath;
+    std::vector<Choice> choices;
+};
+
+// Class representing the visual novel
+class VisualNovel {
+public:
+    void run();
+    
+private:
+    void displayScene(const Scene& scene);
+    int getPlayerChoice();
+    
+    std::vector<Scene> scenes_;
+};
+
+#endif
